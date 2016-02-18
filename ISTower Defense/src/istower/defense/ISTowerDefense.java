1a722b07042680d244300f4cfb9ee46cc1fe0437
knervous <pcj174@gmail.com>
2016-02-18 10:51:53 -0500

    gaga

--- ISTower Defense/src/istower/defense/ISTowerDefense.java
+++ ISTower Defense/src/istower/defense/ISTowerDefense.java
@@ -2,15 +2,29 @@
 package istower.defense;
 
 import Controllers.MainController;
+import javax.sound.midi.*;
+import java.io.InputStream;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ISTowerDefense {
 
     static MainController mc;
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         
         mc = new MainController();
+        
+        InputStream is;
+        Sequencer sequencer = MidiSystem.getSequencer();
+        sequencer.open();
+        is = new BufferedInputStream(new FileInputStream(new File("src/lady_gaga-bad_romance.mid")));
+        sequencer.setSequence(is);
+        sequencer.start();
+
+
     }
     
 }
