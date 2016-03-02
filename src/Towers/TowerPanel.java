b4723c5991aedf268872d3f34ba4efac32058849
yvc5283 <yvc5283@208-NWD04.up.ist.local>
2016-03-02 15:06:53 -0500

    tower panel

---
+++ src/Towers/TowerPanel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Towers;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author yvc5283
+ */
+public class TowerPanel {
+public class ObjectPanel extends JPanel{
+     private JLabel Name, Range, damage, cost;
+}
+}
