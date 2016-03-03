3b9e011e3aab0e286ac1f1a92bfb28569975e485
knervous <pcj174@gmail.com>
2016-03-03 15:43:21 -0500

    cleaned up

--- test/Levels/LevelTest.java
+++ test/Levels/LevelTest.java
@@ -5,19 +5,10 @@
  */
 package Levels;
 
-import Animations.Projectile;
 import Enemies.*;
 import Towers.*;
 import istower.defense.v1.OptionsPanel;
-import istower.defense.v1.PlayGame;
-import java.awt.Color;
-import java.awt.Graphics;
-import javax.swing.*;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.Random;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -218,79 +209,5 @@ public class LevelTest {
     }
 
 
-
-    public class LevelOne extends Level {
-
-    private int level = 1;
-    private ArrayList<Point> pathingPoints;
-    private Point startingPoint;
-    private String grass;
-    private String path;
-
-    public LevelOne() {
-        super();
-        this.setBackground(Color.black);
-        setLayout(null);
-        path = "Images/dirtpath2.jpg";
-        grass = "Images/forestfloor.jpg";
-        initWorld();
-    }
-
-    private void initWorld() {
-        world.add(new Rectangle(0, 0, 350, 250));      // top left
-        world.add(new Rectangle(0, 300, 300, 300));    // bottom  left
-        world.add(new Rectangle(350, 0, 225, 510));    // top right
-
-    }
-
-    public ArrayList<Point> getPathingPoints() {
-        return pathingPoints;
-    }
-
-    public void setPathingPoints() {
-        pathingPoints = new ArrayList<>();
-        pathingPoints.add(new Point(300, 250));
-        pathingPoints.add(new Point(300, 520));
-        pathingPoints.add(new Point(575, 520));
-        pathingPoints.add(new Point(575, 90));
-    }
-
-    @Override
-    public int getNumEnemies() {
-        return new Random().nextInt(4) + 1;
-    }
-
-    @Override
-    public int getEnemyType() {
-        int rand = new Random().nextInt(8) + 1;
-        if (rand > 3) {
-            return 1;
-        } else {
-            return 2;
-        }
-    }
-
-    @Override
-    public String getGrass() {
-        return grass;
-    }
-
-    @Override
-    public String getPath() {
-        return path;
-    }
-
-    @Override
-    public Point getStartingPoint() {
-
-        return startingPoint;
-    }
-
-    @Override
-    public void setStartingPoint() {
-        startingPoint = new Point(0, 250);
-    }
-
-}
     
 }
